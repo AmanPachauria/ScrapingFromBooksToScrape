0b1eb898e125219483d4250602e3b77be3bad24a
AmanPachauria <98804864+AmanPachauria@users.noreply.github.com>
2023-09-12 16:51:43 +0530

    Delete mongoscript.py

--- mongoscript.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoCliennt
-
-client = MongoClient("mongodb+srv://AmazoneData:AmazoneData@scrapingfromamazone.qhi9me9.mongodb.net/")
-
-db = client.scrapy
-
-collection = db.test_collection
